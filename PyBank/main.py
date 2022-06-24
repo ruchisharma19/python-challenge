bde95cc58f89bc84df2af2e0f461fad4fd0abe2d
ruchisharma19 <skrg.kota@gmail.com>
2022-06-24 19:57:19 +0100

    Writing results to the output

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,7 @@ import csv
 import os
 
 csvpath = os.path.join('Resources','budget_data.csv')
-
-print(csvpath)
-
+outpath = os.path.join('Analysis','financial_analysis.csv')
 
 with open(csvpath) as csvfile:
 
@@ -15,12 +13,16 @@ with open(csvpath) as csvfile:
     total=0
     preval=0
     diffdict = {}
-    
+    sno_list = []
+    yrmon_list = []
+    profitloss_list = []
+    cummttl_list = []
+    incdec_list = []
+     
     for row in csvreader:
         rowcount = rowcount+1
- 
-        total = total + int(row[1])
         
+        total = total + int(row[1])
         change = float(row[1]) - float(preval)
         preval = row[1]
         
@@ -28,21 +30,35 @@ with open(csvpath) as csvfile:
              
         if rowcount == 1:
             change = 0
-    
             strtval = row[1]
 
         diffdict[mon] = change  
-      
-    maxprofit_mon = max(diffdict,key=diffdict.get) 
-    maxloss_mon = min(diffdict,key=diffdict.get)
-    
-    
-    average = round((float(preval) - float(strtval))/(rowcount - 1),2)
-    
-    print("Financial Analysis")
-    print("----------------------------")
-    print(f"Total Months :  {rowcount}")
-    print(f"Total : ${total}")
-    print(f"Average Change : ${average}")
-    print(f"Greatest Increase in Profits : {maxprofit_mon} (${diffdict[maxprofit_mon]})")
-    print(f"Greatest Decrease in Profits : {maxloss_mon} (${diffdict[maxloss_mon]})")
+
+        sno_list.append(rowcount)
+        yrmon_list.append(mon)
+        profitloss_list.append(row[1])
+        cummttl_list.append(total)
+        incdec_list.append(change)
+
+outcsv=zip(sno_list,yrmon_list,profitloss_list,cummttl_list,incdec_list)
+
+with open(outpath, 'w') as outfile:
+    csvwriter = csv.writer(outfile)
+    csvwriter.writerow(['SNO','Year Month','Profit/Loss','Cummulative Total','Increase/Decrease'])
+
+    for rows in outcsv:
+
+        csvwriter.writerow(rows)            
+            
+maxprofit_mon = max(diffdict,key=diffdict.get) 
+maxloss_mon = min(diffdict,key=diffdict.get)
+
+average = round((float(preval) - float(strtval))/(rowcount - 1),2)
+
+print("Financial Analysis")
+print("----------------------------")
+print(f"Total Months :  {rowcount}")
+print(f"Total : ${total}")
+print(f"Average Change : ${average}")
+print(f"Greatest Increase in Profits : {maxprofit_mon} (${diffdict[maxprofit_mon]})")
+print(f"Greatest Decrease in Profits : {maxloss_mon} (${diffdict[maxloss_mon]})")
